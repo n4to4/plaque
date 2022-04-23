9d85fcf6ed763fcc30471f794bc13b1730604ba1
n4to4 <n4to4k@gmail.com>
2022-04-23 10:41:20 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     error::Error,
     io::{Read, Write},
-    net::{SocketAddr, TcpListener},
+    net::{SocketAddr, TcpListener, TcpStream},
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -9,26 +9,36 @@ fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind(addr)?;
     println!("Listening on http://{}", addr);
     loop {
-        let (mut stream, addr) = listener.accept()?;
-        println!("Accepted connection from {addr}");
-
-        let mut incoming = vec![];
-
-        loop {
-            let mut buf = vec![0u8; 1024];
-            let read = stream.read(&mut buf)?;
-            incoming.extend_from_slice(&buf[..read]);
-
-            if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
-                break;
+        let (stream, addr) = listener.accept()?;
+        std::thread::spawn(move || {
+            if let Err(e) = handle_connection(stream, addr) {
+                println!("Error handling connection {addr}: {e}");
             }
-        }
-
-        let incoming = std::str::from_utf8(&incoming)?;
-        println!("Got HTTP request:\n{}", incoming);
-        stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
-        stream.write_all(b"\r\n")?;
-        stream.write_all(b"Hello from plaque!\r\n")?;
-        println!("Closing connection for {addr}");
+        });
     }
+}
+
+fn handle_connection(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+    println!("Accepted connection from {addr}");
+
+    let mut incoming = vec![];
+
+    loop {
+        let mut buf = vec![0u8; 1024];
+        let read = stream.read(&mut buf)?;
+        incoming.extend_from_slice(&buf[..read]);
+
+        if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
+            break;
+        }
+    }
+
+    let incoming = std::str::from_utf8(&incoming)?;
+    println!("Got HTTP request:\n{}", incoming);
+    stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
+    stream.write_all(b"\r\n")?;
+    stream.write_all(b"Hello from plaque!\r\n")?;
+    println!("Closing connection for {addr}");
+
+    Ok(())
 }
