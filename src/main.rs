869926fd03789eb98cdd6a59ac2fa32f714cf114
n4to4 <n4to4k@gmail.com>
2022-04-26 18:34:50 +0900

    axum

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,28 @@
+use axum::{response::IntoResponse, routing::get, Router, Server};
 use std::{error::Error, net::SocketAddr};
-use tokio::{
-    io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
-};
-use tracing::{debug, error, info, info_span, Instrument};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
-use tracing_tree::HierarchicalLayer;
+use tracing::info;
+
+mod tracing_stuff;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let tracer =
-        opentelemetry_jaeger::new_pipeline().install_batch(opentelemetry::runtime::Tokio)?;
-    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-
-    let (console, server) = console_subscriber::ConsoleLayer::builder().build();
-    tokio::spawn(async move {
-        server.serve().await.unwrap();
-    });
-
-    Registry::default()
-        .with(EnvFilter::from_default_env())
-        .with(
-            HierarchicalLayer::new(2)
-                .with_targets(true)
-                .with_bracketed_fields(true),
-        )
-        .with(telemetry)
-        .with(console)
-        .init();
-
+    tracing_stuff::setup()?;
     run_server().await?;
+    tracing_stuff::teardown();
+    Ok(())
+}
 
-    opentelemetry::global::shutdown_tracer_provider();
+async fn run_server() -> Result<(), Box<dyn Error>> {
+    let addr: SocketAddr = "0.0.0.0:3779".parse()?;
+    info!("Listening on http://{}", addr);
+
+    let app = Router::new().route("/", get(root));
+    Server::bind(&addr).serve(app.into_make_service()).await?;
 
     Ok(())
 }
 
 #[tracing::instrument]
-async fn run_server() -> Result<(), Box<dyn Error>> {
-    let addr: SocketAddr = "0.0.0.0:3779".parse()?;
-    info!("Listening on http://{}", addr);
-    let listener = TcpListener::bind(addr).await?;
-    loop {
-        let (stream, addr) = listener.accept().instrument(info_span!("accept")).await?;
-        tokio::spawn(async move {
-            if let Err(err) = handle_connection(stream, addr).await {
-                error!(%err, "Error handling connection");
-            }
-        });
-    }
-}
-
-#[tracing::instrument(skip(stream))]
-async fn handle_connection(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
-    let req = read_http_request(&mut stream).await?;
-    debug!(%req, "Got HTTP request");
-    write_http_response(&mut stream).await?;
-
-    Ok(())
-}
-
-#[tracing::instrument(skip(stream))]
-async fn read_http_request(mut stream: impl AsyncRead + Unpin) -> Result<String, Box<dyn Error>> {
-    let mut incoming = vec![];
-
-    loop {
-        let mut buf = vec![0u8; 1024];
-        let read = stream.read(&mut buf).await?;
-        incoming.extend_from_slice(&buf[..read]);
-
-        if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
-            break;
-        }
-    }
-
-    Ok(String::from_utf8(incoming)?)
-}
-
-#[tracing::instrument(skip(stream))]
-async fn write_http_response(mut stream: impl AsyncWrite + Unpin) -> Result<(), Box<dyn Error>> {
-    stream.write_all(b"HTTP/1.1 200 OK\r\n").await?;
-    stream.write_all(b"\r\n").await?;
-    stream.write_all(b"Hello from plaque!\r\n").await?;
-    Ok(())
+async fn root() -> impl IntoResponse {
+    "Hello from plaque!\n"
 }
