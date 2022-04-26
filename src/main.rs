b3c8dc5f6917840a27244b86191436aa58eb434d
n4to4 <n4to4k@gmail.com>
2022-04-26 18:51:59 +0900

    tower

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use axum::{response::IntoResponse, routing::get, Router, Server};
 use std::{error::Error, net::SocketAddr};
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 use tracing::info;
 
 mod tracing_stuff;
@@ -16,7 +18,11 @@ async fn run_server() -> Result<(), Box<dyn Error>> {
     let addr: SocketAddr = "0.0.0.0:3779".parse()?;
     info!("Listening on http://{}", addr);
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/", get(root)).layer(
+        ServiceBuilder::new()
+            .layer(TraceLayer::new_for_http())
+            .into_inner(),
+    );
     Server::bind(&addr).serve(app.into_make_service()).await?;
 
     Ok(())
